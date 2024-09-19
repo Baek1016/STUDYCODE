c3bd03009907a60fdc998f7437ca0f96cce147c0
Baek1016 <baejjae1016@gmail.com>
2024-09-19 17:44:30 +0900

    이번주 한거

---
+++ Project03/Project1/Example02.cpp
@@ -0,0 +1,20 @@
+/*#include<stdio.h>
+#include<stdint.h>
+
+int main() {
+	char ch = 9;
+	short snum = 3;
+	int inum = 7;
+
+	uint8_t c= 9;
+	uint16_t s=3;
+	uint32_t i = 7;
+
+	printf("char=%d\n", sizeof(ch));
+	printf("short=%d\n", sizeof(snum));
+	printf("int=%d\n", sizeof(inum));
+
+	printf("uint8_t:%d\n", sizeof(c));
+	printf("uint16_t:%d\n", sizeof(s));
+	printf("uint32_t:%d\n", sizeof(i));
+}*/

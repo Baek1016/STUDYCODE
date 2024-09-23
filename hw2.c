f7bba295ba57a25bf3151452593467d1b5d04360
BAEKJAEWON <baejjae1016@gmail.com>
2024-09-23 13:41:55 +0900

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -4,11 +4,11 @@ int main() {
     double km, mile;
 
     printf("변환할 거리를 킬로미터(km) 단위로 입력하세요: ");
-    scanf("%lf", &km);
+    scanf("%3lf", &km);
 
     mile = km / 1.609; // 킬로미터를 마일로 변환
 
-    printf("%.1lf km는 %.1lf mile입니다.\n", km, mile);
+    printf("%.3lf km는 %.3lf mile입니다.\n", km, mile);
 
     return 0;
 }

f60dd5b1d553867c1bb8e17769a6a8c55453b664
BAEKJAEWON <baejjae1016@gmail.com>
2024-11-07 11:28:34 +0900

    업데이트

---
+++ hw6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5];
+    int i;
+
+    printf("Please input five integers: ");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("Odd numbers: ");
+    for (i = 0; i < 5; i++) {
+        if (numbers[i] % 2 != 0) {
+            printf("%d ", numbers[i]);
+        }
+    }
+
+    printf("\nEven numbers: ");
+    for (i = 0; i < 5; i++) {
+        if (numbers[i] % 2 == 0) {
+            printf("%d ", numbers[i]);
+        }
+    }
+
+    return 0;
+}
